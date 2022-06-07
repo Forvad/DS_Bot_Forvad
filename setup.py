80793ad1143d4d12c9efae3e2ddee1f8b2bc3309
Forvard <ficaliighvdrb@gmail.com>
2022-06-07 22:56:44 +0300

    update 0.1.1
    red setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def setup():
     sleep(3)
     if 'posix' in name:
         system("pip3 install requests==2.25.1 loguru==0.5.3 urllib3==1.26.8 "
-               "TgCrypto==1.2.3 gunicorn==20.1.0 PySocks==1.7.1 pyTelegramBotAPI==4.3.1 configparser")
+               "TgCrypto==1.2.3 gunicorn==20.1.0 PySocks==1.7.1 pyTelegramBotAPI==4.3.1 configparser msvcrt")
     else:
         system("pip install requests==2.25.1 loguru==0.5.3 urllib3==1.26.8 "
                "TgCrypto==1.2.3 gunicorn==20.1.0 PySocks==1.7.1 pyTelegramBotAPI==4.3.1 configparser msvcrt")
